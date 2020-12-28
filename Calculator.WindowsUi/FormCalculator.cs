5a070a8a08a7381cd96c6507ff0a85178094a46b
lescalona <lescalona@aadi-capif.org.ar>
2020-12-28 19:06:35 -0300

    Inicio Calculadora

---
+++ Calculator.WindowsUi/FormCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Calculator.WindowsUi
+{
+    public partial class FormCalculator : Form
+    {
+        public FormCalculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
